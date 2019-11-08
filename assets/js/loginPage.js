dade7238bf9249cdb87cbbb3627be49f44e6a47f
VToreZ <lumisitone@gmail.com>
2019-11-08 16:17:04 +0300

    Login page inputs logic added(wo phone mask)

--- assets/js/loginPage.js
+++ assets/js/loginPage.js
@@ -1,7 +1,11 @@
-selector = document.querySelector('.form_default--country')
+const formCountry = document.querySelector('.form_default--country');
+const formPhone = document.querySelector('.form_default--phone');
+const countryItem = document.querySelector(".countryItem");
+const countryItemArr = document.querySelectorAll(".countryItem");
+const country_list = document.querySelector(".country_list");
 
-selector.addEventListener("change", () => {
-  if (selector.value) {
+formCountry.addEventListener("change", () => {
+  if (formCountry.value) {
     let arr = document.querySelectorAll(".inputLabel");
     arr.forEach((elem) => {elem.style.visibility = "visible"});
   } else {
@@ -9,3 +13,24 @@ selector.addEventListener("change", () => {
     arr.forEach((elem) => {elem.style.visibility = "hidden"});
   }
 }); // NOTE: Lable control
+
+formCountry.addEventListener("focus", () => {
+  country_list.style.visibility = "visible";
+  country_list.style.opacity = "1";
+});
+formCountry.addEventListener("blur", () => {
+  country_list.style.opacity = "0";
+  country_list.style.visibility = "hidden";
+}); // NOTE: Country List visibility;
+
+country_list.addEventListener("mousedown", () => {
+  formCountry.value = event.target.childNodes[1].innerHTML;
+  formPhone.value = event.target.childNodes[2].innerHTML;
+  let len = event.target.childNodes[2].innerHTML.length - 1;
+  formPhone.pattern = `\\+[0-9]{${len}}[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`;
+}); // NOTE: Country input value & Phone input value
+
+countryItemArr.forEach((elem) => (elem.addEventListener("mouseover", () => {
+  formCountry.value = "";
+  formCountry.placeholder = event.target.childNodes[1].innerHTML;
+}))); // NOTE: Country input placeholder

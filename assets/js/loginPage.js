bbe1d7262db7b512afc5c0ee875831234e6d90bb
VToreZ <lumisitone@gmail.com>
2019-11-19 23:48:06 +0300

    registration frontend logic

--- assets/js/loginPage.js
+++ assets/js/loginPage.js
@@ -4,6 +4,24 @@ const formSubmit = document.querySelector('.form_default--next');
 const countryItem = document.querySelector(".countryItem");
 const countryItemArr = document.querySelectorAll(".countryItem");
 const country_list = document.querySelector(".country_list");
+const inputLabels = document.querySelectorAll(".signPage .inputLabel");
+
+/////////////////////////////////////////////////////// CODE PAGE
+
+const codeLabel = document.querySelector(".codeLabel");
+/////////////////////////////////////////////////////// PASS PAGE
+
+const passPage = document.querySelector(".passPage");
+const passInput = document.querySelector(".form_default--pass");
+const passShowButton = document.querySelector(".input_show_button");
+const passLabel = document.querySelector(".passLabel");
+const passNext = document.querySelector(".passNext");
+/////////////////////////////////////////////////////// NAME PAGE
+
+const namePage = document.querySelector(".namePage");
+const nameInput = document.querySelector(".form_default--name");
+const lastNameInput = document.querySelector(".form_default--lastName");
+
 // const country_item = document.querySelector()
 let len = 0;
 let addingNum = "";
@@ -54,14 +72,40 @@ function checkAddingNum(key) {
   return (key >= '0' && key <= '9') || key == 'ArrowRight' || key == 'Delete';
 };
 
+function checkCode(key) { //CODE
+  if (key === "Enter") {
+
+    if (!codeValid()) {
+      console.log("ccc");
+      codeInput.style.boxShadow = "0 0 0 1px red";
+      codeInput.style.borderColor = "red";
+      codeLabel.style.opacity = 1;
+    }
+    return false;
+
+  }
+  return (key >= '0' && key <= '9') || key == 'ArrowLeft' || key == 'ArrowRight' || key == 'Delete' || key == 'Backspace';
+}
+
+function codeValid(code) {
+  return (code == 12312);
+}
+
+function phoneNumValid(number) {
+  return (number.match(`\\+[0-9]{${len}}\\s[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`));
+}
+
+formCountry.onkeydown = (event) => {
+  event.preventDefault();
+}
 
 formCountry.addEventListener("input", () => {
   if (formCountry.value) {
     let arr = document.querySelectorAll(".inputLabel");
-    arr.forEach((elem) => {elem.style.visibility = "visible"});
+    arr.forEach((elem) => {elem.style.opacity = 1});
   } else {
     let arr = document.querySelectorAll(".inputLabel");
-    arr.forEach((elem) => {elem.style.visibility = "hidden"});
+    arr.forEach((elem) => {elem.style.opacity = 0});
   }
 }); // NOTE: Lable control
 
@@ -83,8 +127,7 @@ country_list.addEventListener("mousedown", () => {
   formCountry.value = event.target.childNodes[1].innerHTML;
   formPhone.value = event.target.childNodes[2].innerHTML + " ";
 
-  let arr = document.querySelectorAll(".inputLabel");
-  arr.forEach((elem) => {elem.style.visibility = "visible"});
+  inputLabels.forEach((elem) => {elem.style.opacity = "1"});
   len = event.target.childNodes[2].innerHTML.length - 1;
   addingNum = event.target.childNodes[2].innerHTML + " ";
   lang = phoneFormat.ru;
@@ -108,11 +151,27 @@ formPhone.addEventListener("keydown", (event) => {
   if (checkPhoneKey(event.key, "ru")) {
     return true;
   }
+  if (event.key === "Enter") {
+    if (phoneNumValid(formPhone.value)) {
+      let phoneNum = formPhone.value;
+      document.querySelector(".codePage").style.visibility = "visible";
+      document.querySelector(".headerNum").innerText = phoneNum;
+      document.querySelector(".signPage").style.visibility = "hidden";
+      formSubmit.style.visibility = "hidden";
+      event.preventDefault();
+      codeInput.focus();
+    }
+    // else {
+    //   formPhone.style.border = "1px solid red"
+    //   formPhone.style.boxShadow = "0 0 0 1px red";
+    //   console.log("asdsad");
+    // }
+  }
   event.preventDefault();
 });
 
 formPhone.addEventListener("input", () => {
-  if (formPhone.value.match(`\\+[0-9]{${len}}\\s[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`)) {
+  if (phoneNumValid(formPhone.value)) {
     formSubmit.style.visibility = "visible";
     formSubmit.style.opacity = 1;
   } else {
@@ -122,25 +181,157 @@ formPhone.addEventListener("input", () => {
 });
 
 formPhone.addEventListener("submit", () => {
-  if (!formPhone.value.match(`\\+[0-9]{${len}}\\s[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`)) {
+  if (phoneNumValid(formPhone.value)) {
     formPhone.style.border = "1px solid red";
     console.log("asdsad");
   }
 });
 
 formSubmit.addEventListener("transitionend", () => {
-  if (!formPhone.value.match(`\\+[0-9]{${len}}\\s[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`)) {
+  if (!phoneNumValid(formPhone.value)) {
     formSubmit.style.visibility = "hidden";
   }
 });
+const codeInput = document.querySelector(".form_default--code");
 
 formSubmit.addEventListener("mousedown", () => {
-  if (formPhone.value.match(`\\+[0-9]{${len}}\\s[0-9]{3}\\s[0-9]{3}\\s[0-9]{2}\\s[0-9]{2}`)) {
-    document.querySelectorAll(".codePage").forEach((current) => {
-      current.style.visibility = "visible";
-    });
+  if (phoneNumValid(formPhone.value)) {
+    let phoneNum = formPhone.value;
+    document.querySelector(".codePage").style.visibility = "visible";
+    document.querySelector(".headerNum").innerText = phoneNum;
+    document.querySelector(".signPage").style.visibility = "hidden";
+    formSubmit.style.visibility = "hidden";
+    event.preventDefault();
+    codeInput.focus();
   } else {
-    formPhone.style.border = "1px solid red"
+    formPhone.style.border = "1px solid red";
     console.log("asdsad");
   }
+});  //PAGE 2
+
+// let codeValid = false;
+
+codeInput.addEventListener("keydown", (event) => {
+  if (checkCode(event.key)) {
+    return true;
+  }
+  event.preventDefault();
+})
+
+//
+codeInput.addEventListener("submit", (event) => {
+  // event.preventDefault();
+  if (codeValid(codeInput.value)) {
+    //NEXT PAGE
+  } else {
+    console.log("ccc");
+    codeInput.style.boxShadow = "0 0 0 1px red";
+    codeInput.style.borderColor = "red";
+
+    //border: 1px solid $textActiveColor
+    //box-shadow: 0 0 0 1px $textActiveColor
+  }
+  return false;
 });
+//
+codeInput.addEventListener("input", (event) => {
+  codeInput.style.boxShadow = "0 0 0 1px #4ea4f6";
+  codeInput.style.borderColor = "#4ea4f6";
+  codeLabel.style.opacity = 0;
+  if (codeInput.value.length == 5) {
+    if (codeValid(codeInput.value)) {
+      document.querySelector(".codePage").style.visibility = "hidden";
+      document.querySelector('.passPage').style.visibility = "visible";
+      passNext.style.visibility = "visible";
+      passNext.style.opacity = "1";
+      passInput.focus();
+    } else {
+      codeInput.style.boxShadow = "0 0 0 1px red";
+      codeInput.style.borderColor = "red";
+      codeLabel.style.opacity = 1;
+    };
+
+}});
+
+/////////////////////////////////////////////////////// PASS PAGE
+
+let passValid = (pass) => pass === "123123";
+
+passShowButton.onclick = () => {
+  console.log("xxx");
+  if (passInput.type === "password") {
+    passInput.type = "text";
+    passShowButton.src = "./Design/Icons/eye2_svg.svg";
+    passInput.focus();
+  } else {
+    passInput.type = "password";
+    passShowButton.src = "./Design/Icons/eye1_svg.svg";
+    passInput.focus();
+  }
+}
+
+passInput.oninput = () => {
+  if (passInput.value) {
+    document.querySelector(".passLabel").style.opacity = "1";
+  } else {
+    document.querySelector(".passLabel").style.opacity = "0";
+  }
+
+  if (passLabel.innerHTML == "Invalid Password") {
+    passInput.style.boxShadow = "0 0 0 1px #4ea4f6";
+    passInput.style.borderColor = "#4ea4f6";
+    passLabel.innerHTML = "Password";
+    passLabel.style.color = "#4ea4f6";
+  }
+}
+
+passInput.onkeydown = (event) => {
+  if (event.key === "Enter") {
+    if (passValid(passInput.value)) {
+      passPage.style.visibility = "hidden";
+      passNext.style.visibility = "hidden";
+      namePage.style.visibility = "visible";
+      event.preventDefault();
+      nameInput.focus();
+    } else {
+      passInput.style.boxShadow = "0 0 0 1px red";
+      passInput.style.borderColor = "red";
+      passLabel.style.color = "red";
+      passLabel.innerHTML = "Invalid Password";
+    }
+  }
+}
+
+passNext.onmousedown = (event) => {
+  if (passValid(passInput.value)) {
+    passPage.style.visibility = "hidden";
+    passNext.style.visibility = "hidden";
+    namePage.style.visibility = "visible";
+    event.preventDefault();
+    nameInput.focus();
+
+  } else {
+    passInput.style.boxShadow = "0 0 0 1px red";
+    passInput.style.borderColor = "red";
+    passLabel.style.color = "red";
+    passLabel.innerHTML = "Invalid Password";
+  }
+}
+
+/////////////////////////////////////// PAGE NAME
+
+nameInput.oninput = () => {
+  if (nameInput.value) {
+    document.querySelector(".nameLabel").style.opacity = "1";
+  } else {
+    document.querySelector(".nameLabel").style.opacity = "0";
+  }
+};
+
+lastNameInput.oninput = () => {
+  if (lastNameInput.value) {
+    document.querySelector(".lastNameLabel").style.opacity = "1";
+  } else {
+    document.querySelector(".lastNameLabel").style.opacity = "0";
+  }
+};
